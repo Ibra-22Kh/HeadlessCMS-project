e48022dddb90d2388bc341a52f019d4133e2857f
ibra-kdbra <sonarfarouq@gmail.com>
2023-05-11 23:48:41 +0300

    many updates and insertion

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter, RouterProvider, Route, Outlet } from "react-router-dom";
-import Navbar from "./components/Navbar"
-import Footer from "./components/Footer"
+import Navbar from "./components/Navbar/Navbar"
+import Footer from "./components/Footer/Footer"
 import Home from "./pages/Home/Home";
 import Product from "./pages/Product/Product";
 import Products from "./pages/Products/Products";
