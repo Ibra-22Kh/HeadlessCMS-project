edcf9dab5d7d6d6c67d5e02b7cbee74695faf216
ibra-kdbra <sonarfarouq@gmail.com>
2023-05-14 00:47:26 +0300

    update

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
       <div className="wrapper">
         <div className="left">
           <div className="item">
-            <img src="/img/en.png" alt="" />
+            <img src="images/en.png" alt="" />
             <KeyboardArrowDownIcon />
           </div>
           <div className="item">
@@ -36,7 +36,7 @@ const Navbar = () => {
           </div>
         </div>
         <div className="center">
-          <Link className ="link" to="/">LAMASTORE</Link>
+          <Link className ="link" to="/">E-STORE</Link>
         </div>
         <div className="right">
           <div className="item">
