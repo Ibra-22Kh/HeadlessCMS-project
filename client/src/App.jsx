b29850b87d4b7dcdb2355a84ce5901a9075d5558
ibra-kdbra <sonarfarouq@gmail.com>
2023-05-13 15:17:43 +0300

    main

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,40 +1,41 @@
-import { createBrowserRouter, RouterProvider, Route, Outlet } from "react-router-dom";
-import Navbar from "./components/Navbar/Navbar"
-import Footer from "./components/Footer/Footer"
+import { Children } from "react";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import Product from "./pages/Product/Product";
 import Products from "./pages/Products/Products";
-import "./App.scss";
+import "./app.scss"
 
-const Layout = ()=>{
+const Layout = () => {
   return (
     <div className="app">
-      <Navbar/>
-      <Outlet/>
-      <Footer/>
+      <Navbar />
+      <Outlet />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
 const router = createBrowserRouter([
   {
-    path:"/",
-    element:<Layout/>,
-    children:[
+    path: "/",
+    element: <Layout />,
+    children: [
       {
-        path:"/",
-        element:<Home/>
+        path: "/",
+        element: <Home />,
       },
       {
-        path:"/products/:id",
-        element:<Products/>
+        path: "/products/:id",
+        element: <Products />,
       },
       {
-        path:"/product/:id",
-        element:<Product/>
+        path: "/product/:id",
+        element: <Product />,
       },
-    ]
-  }
+    ],
+  },
 ]);
 
 function App() {
